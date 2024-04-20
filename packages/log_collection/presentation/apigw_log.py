986070bd3c71428728ba51ec1fc3cf43be4edd50
Tadashi Takahashi <ttakahashi@umn.co.jp>
2024-04-21 00:20:58 +0900

    batchを引数で指定したバッチタイプに対応する処理を実行するように修正

--- packages/log_collection/presentation/apigw_log.py
+++ packages/log_collection/presentation/apigw_log.py
@@ -1,20 +1,31 @@
-"""_summary_
-"""
-
+import argparse
 from datetime import datetime
+from enum import Enum
 
 from packages.log_collection.usecase.apigw_log.apigw_log import ApigwLog
 
 
-def main():
-    """_summary_"""
-    start_date = datetime(2024, 2, 1)
-    end_date = datetime(2024, 4, 17)
-    marker = ""
+class BatchType(Enum):
+    FETCH_LOG = "fetch_log"
+    LOAD_LOG = "load_log"
 
+
+def execute(batch_type: BatchType):
+    """execute batch process designated by batch_type"""
     apigw_log = ApigwLog()
-    apigw_log.fatch_from_s3("prod", start_date, end_date, marker)
+    if batch_type == BatchType.FETCH_LOG:
+        start_date = datetime(2024, 1, 1)
+        end_date = datetime(2024, 4, 17)
+        marker = ""
+
+        apigw_log.fatch_from_s3("prod", start_date, end_date, marker)
+    elif batch_type == BatchType.LOAD_LOG:
+        apigw_log.load_to_db()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--type", required=True)
+    args = parser.parse_args()
+
+    execute(BatchType(args.type))
