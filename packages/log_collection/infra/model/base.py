2fbf06a032cacd0cde8ed5ce12aa7de464331609
Tadashi Takahashi <ttakahashi@umn.co.jp>
2024-04-21 21:19:35 +0900

    SQLAlchemy2.0の警告の対応

--- packages/log_collection/infra/model/base.py
+++ packages/log_collection/infra/model/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 convention = {
     "ix": "ix_%(column_0_label)s",
